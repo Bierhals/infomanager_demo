03860039fadfb7b2742c7b0d6342fe28f1ba8ef4
Sven-Uwe Bierhals <bierhals.sven@mcpaper.de>
2019-08-28 08:44:42 +0200

    Anzeige für Kunden (Mitarbeiter / Filiale) aufgenommen

--- bootstrap-demo/gulpfile.js
+++ bootstrap-demo/gulpfile.js
@@ -4,8 +4,8 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const merge = require('merge-stream');
 
-//move js files to the dist-directory
-gulp.task('js', function () {
+// move js files to the dist-directory
+gulp.task('js', () => {
   const bootstrap = gulp.src('node_modules/bootstrap/dist/js/bootstrap.min*')
     .pipe(gulp.dest('dist/js/bootstrap'));
   const popper = gulp.src('node_modules/popper.js/dist/umd/popper.min*')
@@ -18,45 +18,35 @@ gulp.task('js', function () {
   return merge(bootstrap, popper, jquery, fontawesome);
 });
 
-//move webfonts files to the dist-directory
-gulp.task('webfonts', function () {
-  return gulp.src(['node_modules/@fortawesome/fontawesome-free/webfonts/*'])
-    .pipe(gulp.dest('dist/webfonts'));
-});
+// move webfonts files to the dist-directory
+gulp.task('webfonts', () => gulp.src(['node_modules/@fortawesome/fontawesome-free/webfonts/*'])
+  .pipe(gulp.dest('dist/webfonts')));
 
-//convert scss files to css
-gulp.task('sass', function () {
-  return gulp.src('src/scss/*.scss')
-    .pipe(sass())
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('dist/css'))
-    .pipe(browserSync.stream());
-});
+// convert scss files to css
+gulp.task('sass', () => gulp.src('src/scss/*.scss')
+  .pipe(sass())
+  .pipe(autoprefixer())
+  .pipe(gulp.dest('dist/css'))
+  .pipe(browserSync.stream()));
 
-//move html files to the dist-directory
-gulp.task('html', function () {
-  return gulp.src('./src/**/*.html', { 'base': './src'})
-    .pipe(gulp.dest('dist'));
-});
+// move html files to the dist-directory
+gulp.task('html', () => gulp.src('./src/**/*.html', { base: './src' })
+  .pipe(gulp.dest('dist')));
 
-//move images to the dist-directory
-gulp.task('img', function () {
-  return gulp.src('./src/img/**/*', { 'base': './src'})
-    .pipe(gulp.dest('dist'));
-});
+// move images to the dist-directory
+gulp.task('img', () => gulp.src('./src/img/**/*', { base: './src' })
+  .pipe(gulp.dest('dist')));
 
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
   gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], gulp.series('sass'));
   gulp.watch('src/**/*.html', gulp.series('html')).on('change', browserSync.reload);
 });
 
-//server implementation
-gulp.task('serve', function () {
-  browserSync.init({
-    server: "./dist"
-  });
-});
+// server implementation
+gulp.task('serve', () => browserSync.init({
+  server: './dist',
+}));
 
-//start server
+// start server
 gulp.task('default', gulp.series('js', 'webfonts', 'img', 'sass', 'html', gulp.parallel('watch', 'serve')));
